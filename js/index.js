7516012c1f04f935df57a8c73b9e2197f3ee8f15
Marjon Cajocon <marjoncajocon08@gmail.com>
2024-02-05 13:14:01 +0000

    update info

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
-(function() {
-    
-})();
+function main() {
+	var sider = document.getElementById('sider'); 
+	var close = document.getElementById('sider-close');
+	var profile_pic = document.getElementById('profile-icon');	
+	
+	var xsider = {
+		show: function() {
+			sider.style.display = 'block';
+		},
+		close: function() {
+			sider.style.display = 'none';
+		}
+	};	
+
+	profile_pic.addEventListener('click', function() {
+		xsider.show();
+	});
+
+	close.addEventListener('click', function() {
+		xsider.close();
+	});
+
+	var loadContent = function(page) {
+		$.post(page).then(function(o) {
+			$('.content').html(o);
+		});
+	};
+	
+	loadContent('about.html');
+	
+};
+main();
